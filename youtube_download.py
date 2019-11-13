f7963b8052a3cfa1bdc9204882749025f21de89d
shinley <chenxl4@autoai.com>
2019-11-13 19:54:41 +0800

    youtube-dl方式

---
+++ youtube_download.py
@@ -0,0 +1,26 @@
+from __future__ import unicode_literals
+import youtube_dl
+
+
+def my_hook(d):
+    print(d)
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+
+
+ydl_opts = {
+    'proxy':'http://127.0.0.1:7890',
+    'progress_hooks': [my_hook],
+    'nocheckcertificate': True
+}
+
+with open('video_url.txt', 'r') as video_url:
+    while True:
+        line = video_url.readline()
+        if not line:
+            break
+
+        url = "https://www.youtube.com" + line
+
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
