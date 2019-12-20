3488fed8bda8237c8ad9c133e2313d290e7060ae
shinley <chenxl4@autoai.com>
2019-12-20 20:52:26 +0800

    加字幕

--- insert_srt_logo.py
+++ insert_srt_logo.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import sys
 
 source_path = "/Volumes/pan/fix_video/rough_video/"
 srt_path = "/Volumes/pan/fix_video/vanessa_srt/"
@@ -10,7 +11,7 @@ files = os.listdir(source_path)
 
 
 for file in files:
-    cmd = "ffmpeg -i '%s' -vf \"movie=%s[logo];[in][logo]overlay=x='if(gte(t\,2)\,((t-2)*80)-w\,NAN)'\",subtitles='%s' '%s'"
+    cmd = "source /etc/profile &&  ffmpeg -i '%s' -vf \"movie=%s[logo];[in][logo]overlay=x='if(gte(t\,2)\,((t-2)*80)-w\,NAN)'\",subtitles='%s' '%s'"
     if file.endswith(".mp4"):
         print(file)
 
@@ -22,11 +23,13 @@ for file in files:
         out_file = dest_path + file
 
         cmd = cmd % (mp4_src, logo_path, srt_file, out_file)
+        sys.stdout.flush()
         ret = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
-        # for line in iter(ret.stdout.readline, b''):
-        #     line = line.decode('utf8')
-        #     print(line)
+        for line in iter(ret.stdout.readline, b''):
+            line = line.decode('utf8')
+            print(line)
 
+        ret.stdout.close()
         # wait finished
-        ret_out = ret.stdout.read().decode('utf8')
-        print(ret_out)
+        # ret_out = ret.stdout.read().decode('utf8')
+        # print(ret_out)
